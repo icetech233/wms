b5550b7a3eabad6d91502d0af4738e5e690cf51a
icetech233 <cnzjhanzhouhb@hotmail.com>
2024-02-26 01:55:49 +0800

    补充 反射

--- util/copy.go
+++ util/copy.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"fmt"
 	"reflect"
+	"strconv"
 	"time"
 )
 
@@ -48,15 +49,20 @@ func copyVal(dfv reflect.Value, sfv reflect.Value) error {
 	st := sfv.Type()
 	// st.AssignableTo()
 	// fmt.Println("dt.Kind()", dt.Kind()) fmt.Println("st.Kind()", st.Kind())
-	// sfv.CanInt() // case Int, Int8, Int16, Int32, Int64:
 	if st.Kind() == dt.Kind() {
 		dfv.Set(sfv)
+		return nil
 	} else {
 		// 不一样的
-		if st.Kind() == reflect.Struct {
-			// fmt.Println("特殊123类型", st.String(), st.Name()) 特殊123类型 time.Time Time
-			if st.String() == "time.Time" {
-				if dt.Kind() == reflect.String {
+		if dt.Kind() == reflect.String {
+			switch st.Kind() {
+			case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+				intStr := strconv.FormatInt(sfv.Int(), 10)
+				dfv.SetString(intStr)
+				return nil
+			case reflect.Struct:
+				// // fmt.Println("特殊123类型", st.String(), st.Name()) 特殊123类型 time.Time Time
+				if st.String() == "time.Time" {
 					// sfv.Pointer()
 					time1 := sfv.Interface().(time.Time)
 					// 结构体不能这么写 fff := *(*time.Time)(sfv.UnsafePointer())
@@ -64,10 +70,20 @@ func copyVal(dfv reflect.Value, sfv reflect.Value) error {
 					time1Str := time1.Format("2006-01-02 15:04:05")
 					dfv.SetString(time1Str)
 				}
+				return nil
+			case reflect.Bool:
+				bool1 := sfv.Bool()
+				if bool1 {
+					dfv.SetString("true")
+				} else {
+					dfv.SetString("false")
+				}
+				return nil
+			default:
+				return nil
 			}
 		}
 	}
-
 	return nil
 }
 
