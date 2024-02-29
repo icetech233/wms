efa14bf5fbdf09b345b7998a81e4554730027e18
icetech233 <cnzjhanzhouhb@hotmail.com>
2024-03-01 03:01:07 +0800

    fontend

--- fontend/MyAntDesignApp2/Pages/Settings/Attrs/Attrs.razor.cs
+++ fontend/MyAntDesignApp2/Pages/Settings/Attrs/Attrs.razor.cs
@@ -1,12 +1,42 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Runtime.Intrinsics.X86;
+using System.Threading.Tasks;
 using AntDesign;
 using Microsoft.AspNetCore.Components;
+using MyAntDesignApp2.Models;
 
 namespace MyAntDesignApp2.Pages.Settings
 {
 
     public partial class Attrs
     {
-        private string _selectKey = "base";
+        private string _attrPageKey = "规格如下:";
+
+        Attr[] attrList;
+
+        [Inject]
+        public HttpClient hc { get; init; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            await base.OnInitializedAsync();
+
+            string requestUri = "http://hw.acgzj.cn:8081" +
+            "/api/v1/attr/list?s" + +Random.Shared.Next(int.MaxValue);
+            var resp = await hc.GetFromJsonAsync<AttrResp>(requestUri);
+
+            Console.WriteLine(resp);
+            if (resp.Code!= 200){
+                // 弹窗 resp.Msg;
+                return;
+            }
+
+            attrList = resp.Data;
+
+
+        }
     }
 }
