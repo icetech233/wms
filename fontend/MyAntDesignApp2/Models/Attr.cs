8a29f6698b98b926cdf9da853bf6e5aaf13c4907
hanbin <hanbin@jiuxiniot.com>
2024-03-01 10:33:56 +0800

    attr

---
+++ fontend/MyAntDesignApp2/Models/Attr.cs
@@ -0,0 +1,11 @@
+﻿namespace MyAntDesignApp2.Models
+{
+    public class Attr
+    {
+        public long AttrID { get; set; }
+        public string AttrName { get; set; }
+        public int ShowType { get; set; }
+        public string CreatedAt { get; set; }
+        public string UpdatedAt { get; set; }
+    }
+}
---
+++ fontend/MyAntDesignApp2/Models/AttrResp.cs
@@ -0,0 +1,15 @@
+﻿namespace MyAntDesignApp2.Models
+{
+    public class AttrResp : BaseResp
+    {
+        public object Debug { get; set; }
+        public Attr[] Data { get; set; }
+    }
+
+    public class BaseResp
+    {
+        public int Code { get; init; }
+        public string Msg { get; init; }
+    }
+
+}
