686ec548d8b0a2a3a8058643b0f5287a1a92bcd5
icetech233 <cnzjhanzhouhb@hotmail.com>
2024-03-03 00:36:33 +0800

    11

---
+++ fontend/MyAntDesignApp2/Models/Attr.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel;
+
+namespace MyAntDesignApp2.Models
+{
+    public class Attr
+    {
+        public long AttrID { get; set; }
+        public string AttrName { get; set; }
+        [DisplayName("创建")]
+        public string CreatedAt { get; set; }
+        [DisplayName("修改")]
+        public string UpdatedAt { get; set; }
+    }
+}
---
+++ fontend/MyAntDesignApp2/Models/AttrResp.cs
@@ -0,0 +1,5 @@
+﻿namespace MyAntDesignApp2.Models
+{
+    public record class BaseResp(int Code, string Msg, object Debug, object Data);
+    public record class AttrResp(int Code, string Msg, object Debug, Attr[] Data);
+}
