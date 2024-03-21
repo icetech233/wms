9222d65a6ce33bf14e4c9841df1edf5810a45f5d
hanbin <hanbin@jiuxiniot.com>
2024-03-21 20:46:56 +0800

    1

--- fontend/MyAntDesignApp2/Program.cs
+++ fontend/MyAntDesignApp2/Program.cs
@@ -17,8 +17,7 @@ namespace WmsApp
 
             // /data/menu.json
             // BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)
-             
-            builder.Services.AddScoped(sp => new HttpClient 
+            builder.Services.AddScoped(sp => new HttpClient
             {
                 BaseAddress = new Uri("http://hw.acgzj.cn:8081/")
             });
