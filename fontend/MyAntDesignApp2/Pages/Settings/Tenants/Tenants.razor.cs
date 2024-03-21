8585696a7bde7cb7ce6d30cafa13d7a476b4183b
hanbin <hanbin@jiuxiniot.com>
2024-03-21 20:23:21 +0800

    1

--- fontend/MyAntDesignApp2/Pages/Settings/Tenants/Tenants.razor.cs
+++ fontend/MyAntDesignApp2/Pages/Settings/Tenants/Tenants.razor.cs
@@ -19,16 +19,24 @@ namespace MyAntDesignApp2.Pages.Settings
         string size = "middle";
         bool _visible = false;
         string modalTitle = "添加"; // 编辑
+
+        private Tenant[] tenantAllList;
         private Tenant[] tenantList;
 
         [Inject]
         public HttpClient hc { get; set; }
 
+        public EventCallback<string> OnChangeHandle;
+
         protected override async Task OnInitializedAsync()
         {
             await base.OnInitializedAsync();
+            await RefreshTableData();
 
-            await Refresh();
+            OnChangeHandle = new EventCallback<string>(null, () =>
+            {
+                RefreshTableDataWithEntType(bindEntType);
+            });
         }
 
         private TenantModel model = new();
@@ -36,17 +44,37 @@ namespace MyAntDesignApp2.Pages.Settings
         {
         }
 
-        private async Task Refresh()
+        private async Task RefreshTableData()
         {
-            string requestUri = "/api/v1/tenant/list?s=" + Random.Shared.Next(int.MaxValue);
-            TenantResp resp = await hc.GetFromJsonAsync<TenantResp> (requestUri);
+            string requestUri = $"/api/v1/tenant/list?ent_type={bindEntType}&s={Random.Shared.Next(int.MaxValue)}";
+            TenantResp resp = await hc.GetFromJsonAsync<TenantResp>(requestUri);
             Console.WriteLine($"refresh code {resp.Code} count {resp.Data?.Length}");
             if (resp.Code != 200)
             {
                 await Notice.Error(_notice, resp.Msg);
             }
-            tenantList = resp.Data;
+            if (tenantAllList == null || tenantAllList.Length == 0)
+            {
+                tenantAllList = resp.Data;
+            }
+            tenantList = [.. tenantAllList];
         }
+
+        private void RefreshTableDataWithEntType(string ent_type)
+        {
+            if ("supplier" == ent_type)
+            {
+                tenantList = tenantAllList.Where(a => a.IsSupplier).ToArray();
+            }
+            else if ("customer" == ent_type)
+            {
+                tenantList = tenantAllList.Where(a => a.IsCustomer).ToArray();
+            }
+            else {
+                tenantList = [.. tenantAllList];
+            }
+        }
+
 
         private void Delete(long id)
         {
